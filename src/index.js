4c3749f6628c79112b84ef05f4c6cdb050d7f52c
sbalayan1 <balayans2014@yahoo.com>
2022-11-15 08:41:48 -0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,15 +1,72 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  addFormDisplayEvent()
+  addFormSubmitEvent()
+  fetchToys()
+});
+
+
+function addFormDisplayEvent() {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
+    toyFormContainer.style.display = addToy ? "block" : "none"
   });
-});
+}
+
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(toy => {renderToy(toy)})
+  })
+}
+
+function renderToy(toy) {
+  const container = document.getElementById('toy-collection')
+  const card = document.createElement('div')
+  const header = document.createElement('h2')
+  const img = document.createElement('img')
+  const p = document.createElement('p')
+  const button = document.createElement('button')
+
+  card.classList.add('card')
+  header.textContent = toy.name
+  img.src = toy.image
+  img.classList.add('toy-avatar')
+  p.textContent = `${toy.name} was liked ${toy.likes} times`
+  button.textContent = "<3"
+  button.classList.add('like-btn')
+  button.id = toy.id
+
+  card.append(header, img, p, button)
+  container.append(card)
+}
+
+function addFormSubmitEvent() {
+  const form = document.querySelector('.add-toy-form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const newToy = {
+      "name": e.target["name"].value,
+      "image": e.target["image"].value,
+      "likes": 0
+    }
+    createToy(newToy)
+  })
+}
+
+function createToy(toy) {
+  fetch('http://localhost:3000/toys', {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(res => res.json())
+  .then(data => {renderToy(data)})
+}
